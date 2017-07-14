f589b14d68487ecff39453d58eed4f3261919b7c
Rickdickulous <reihlrw192@gmail.com>
2017-07-14 07:40:43 -0400

    Temp Sensor library and master Arduino sketch.

---
+++ BrewController.ino
@@ -0,0 +1,30 @@
+#include "TempController.h"
+
+int analogPin = 0;
+float Vcounts = 0.0;
+float currentTemp = 0.0;
+
+TempController tempController = TempController();  // temperature control module
+
+bool tempDebug = true;
+
+void setup() 
+{
+  Serial.begin(9600);  // begin serial communication
+  
+}
+
+void loop() 
+{
+  
+  Vcounts = analogRead(analogPin);    // read the input pin
+
+  // get temp in f
+  currentTemp = tempController.convertInputToTemp_f(Vcounts);
+  Serial.print("currentTemp (F) = ");
+  Serial.println(currentTemp);
+  
+  delay(1000);
+  
+  
+}
---
+++ TempController.cpp
@@ -0,0 +1,35 @@
+#include "TempController.h"
+
+TempController::TempController(void)
+{
+
+}
+
+
+float TempController::convertInputToTemp_f(float Vcounts)
+{
+    Rt = R1 / (1024/Vcounts - 1);
+    float temp_c = (-26.65) * log(Rt) + 271.11;
+    float temp_f = temp_c * 1.8 + 32;
+    return temp_f;
+}
+
+
+float TempController::convertInputToTemp_c(float Vcounts)
+{
+    Rt = R1 / (1024/Vcounts - 1);
+    float temp_c = (-26.65) * log(Rt) + 271.11;
+    return temp_c;
+}
+
+
+float TempController::cToF(float temp_c)
+{
+    return temp_c * 1.8 + 32;
+}
+
+
+TempController::~TempController(void)
+{
+
+}
